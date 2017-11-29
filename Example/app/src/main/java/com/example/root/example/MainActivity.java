b845930d146d056ada55028a71d7158ee72cd721
Justin Miller <justin.miller@marquette.edu>
2017-11-29 14:10:07 -0600

    bluetooth example code to get and parse data from gaetway

---
+++ Example/app/src/main/java/com/example/root/example/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.root.example;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import java.io.IOException;
+import java.util.Set;
+import com.example.root.example.ConnectThread;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        final String TAG = "MainActivity";
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                Log.e(TAG, deviceName);
+                Log.e(TAG, deviceHardwareAddress);
+                ConnectThread ct = new ConnectThread(device, mBluetoothAdapter);
+                ct.run();
+            }
+        }
+
+    }
+}
